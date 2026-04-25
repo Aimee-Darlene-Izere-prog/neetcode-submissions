f4697cc4a05bef1b02536d36640da08eba88abd2
Aimee-Darlene-Izere-prog <aimeeizere2028@u.northwestern.edu>
2026-04-24 19:01:54 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # 1. I already have an answer in the hashmap
+        anagrams = {}
+        # 2. Check if the string exist in the hashmap
+        for s in strs:
+            key = ''.join(sorted(s))
+
+            if key not in anagrams:
+                anagrams[key] = []
+
+            anagrams[key].append(s)
+        return list(anagrams.values())
+        
